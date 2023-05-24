ac5d8271af3983da8c3d858adc181b3f2af305bf
ccremadesa <playerzray3@hotmail.com>
2023-05-24 19:37:12 +0200

    1.0

--- src/main/java/es/ieslaverda/demoSpring/repository/UsuarioDBRepository.java
+++ src/main/java/es/ieslaverda/demoSpring/repository/UsuarioDBRepository.java
@@ -5,10 +5,8 @@ import es.ieslaverda.demoSpring.repository.model.MyDataSource;
 import es.ieslaverda.demoSpring.repository.model.Usuario;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,22 +15,73 @@ public class UsuarioDBRepository implements IUsuarioRepository {
 
     @Override
     public Usuario addUsuario(Usuario usuario) throws SQLException {
+
+        int actualizados=0;
+        DataSource ds = MyDataSource.getMySQLDataSource();
+        String sql = "{call insertEmpleado(?,?,?,?,?,?,?,?,?)}";
+
+        try(Connection connection = ds.getConnection();
+            CallableStatement cs = connection.prepareCall(sql);){
+
+            actualizados = cs.executeUpdate(sql);
+
+            return usuario;
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Usuario updateUsuario(Usuario usuario) throws SQLException {
+
+
         return null;
     }
 
     @Override
     public boolean deleteUsuario(int id) throws SQLException {
+
+        int actualizados=0;
+        DataSource ds = MyDataSource.getMySQLDataSource();
+
+        try(Connection connection = ds.getConnection();
+            Statement statement = connection.createStatement();){
+
+            String query = "DELETE FROM empleado WHERE id = '" + id + "'";
+            actualizados = statement.executeUpdate(query);
+            return true;
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+
         return false;
     }
 
     @Override
     public Usuario getUsuarioById(int id) throws SQLException {
-        return null;
+
+        Usuario usuario = null;
+        DataSource ds = MyDataSource.getMySQLDataSource();
+        String query = "select * from empleado where id = ?";
+
+        try(Connection con = ds.getConnection();
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+        ){
+
+            preparedStatement.setInt(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+            if(rs.next()){
+                usuario = new Usuario(rs.getInt("id"),rs.getString("nombre"),
+                        rs.getString("apellidos"));
+            }
+        } catch (SQLException exception){
+            exception.printStackTrace();
+        }
+        return usuario;
     }
 
     public List<Usuario> getAllUsuarios() throws SQLException {
